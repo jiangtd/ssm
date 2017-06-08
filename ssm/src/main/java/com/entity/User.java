5553c3b100b6161f5f78f4fd0af0eb82d36eb6e4
jiangtd <1520230081@qq.com>
2017-06-08 09:42:37 +0800

    添加配置文件

---
+++ ssm/src/main/java/com/entity/User.java
@@ -0,0 +1,34 @@
+package com.entity;
+
+public class User {
+	private String name;  //姓名
+	private String password;  //密码
+	private Integer age;  //年龄
+	private String address;  //地址
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
